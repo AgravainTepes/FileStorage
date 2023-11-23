79a3d8100174eb7a826a84634482b4f45e2e7dff
AgravainTepes <sergeykuznetsov21@gmail.com>
2023-11-23 07:37:26 +0500

    Initial commit

---
+++ src/test/java/com/agravain/filestorage/RESTControllerTest/RESTControllerTest.java
@@ -0,0 +1,38 @@
+package com.agravain.filestorage.RESTControllerTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RESTControllerTest {
+
+
+    @Test
+    @DisplayName("GET /api/upload возвращает HTTP-ответ со статусом 200 OK и сообщением об успешном сохранении.")
+    void handleGetAllTasks_ReturnsValidResponseEntity() {
+        // given
+
+
+        // when
+
+
+        // then
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
